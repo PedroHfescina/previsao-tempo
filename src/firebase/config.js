8ceeb4ed716bdc6dbb42ea13e264a6e23c5a069b
PedroHfescina <pedrohenriquefescina@gmail.com>
2024-09-10 00:51:40 -0300

    projeto: Previsão do Tempo

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKqFINq6JxMZaQoL2us0jLi4bF66062ps",
+  authDomain: "previsao-tempo-7f710.firebaseapp.com",
+  projectId: "previsao-tempo-7f710",
+  storageBucket: "previsao-tempo-7f710.appspot.com",
+  messagingSenderId: "582210110712",
+  appId: "1:582210110712:web:913e70c027b9ab3a47670d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
